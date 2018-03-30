4ae52f02667b82462f4b6a254d397976de88c766
r3dz0r <36861914+r3dz0r@users.noreply.github.com>
2018-03-30 10:18:19 +0300

    подправил скрипт

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 // <script>
+
   var toggleButton = document.querySelector(".search__popup");
   var modalSearch = document.querySelector(".search__form");
 
   toggleButton.addEventListener("click", function(evt) {
     evt.preventDefault();
     modalSearch.classList.toggle("search__form--close");
+  });
 
-  });
-  button.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modalSearch.classList.toggle("search__form--close");
-  });
   // </script>
