a0af51e84bd6be6ae85273bdc9b9aaeca9f2b88d
r3dz0r <36861914+r3dz0r@users.noreply.github.com>
2018-03-30 10:41:38 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-// <script>
+var toggleButton = document.querySelector(".search__popup");
+var modalSearch = document.querySelector(".search__form");
 
-  var toggleButton = document.querySelector(".search__popup");
-  var modalSearch = document.querySelector(".search__form");
-
+if(toggleButton) {
   toggleButton.addEventListener("click", function(evt) {
     evt.preventDefault();
     modalSearch.classList.toggle("search__form--close");
   });
-
-  // </script>
+}
